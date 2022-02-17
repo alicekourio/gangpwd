e1fb64c2df58045238087e30bf888eff81026011
alicekourio <alice.kourio@spendesk.com>
2022-02-17 17:45:41 +0100

    Use  env var

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ fastify.get(
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(process.env.PORT || 3000);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
